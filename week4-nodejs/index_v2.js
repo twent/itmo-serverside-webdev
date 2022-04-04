da77981a8ddb02ec22b48b8053ea1a5ea314e12c
twent <twentprod@gmail.com>
2022-04-04 22:21:11 +0300

    Week4 Update

--- week4-nodejs/index_v2.js
+++ week4-nodejs/index_v2.js
@@ -9,9 +9,6 @@ const CORS = {
 };
 
 Server((req, res) => {
-    //res.writeHead(200, {'Content-Length': 3}); - write status code and headers
-    //res.write('\u0002\u0001\u0021'); - unicode binary write
-    //res.write(Buffer.from([1, 2, 33])); - binary write from buffer
     
     if (req.url === '/ru') {
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8', ...CORS });
@@ -28,10 +25,18 @@ Server((req, res) => {
     } else if (req.url === '/download') {
         res.writeHead(200, {'Content-Disposition': 'attachment; filename="File.txt"'});
         res.write('File\n');
+    } else if (req.url.startsWith('/timer')) {
+        const secs = req.url.substring(1 + req.url.indexOf('?'));
+        return setTimeout(() => res.end(`<h1>Miliseconds: ${secs}ms</h1>\n`), secs);
+    } else if (req.url.startsWith('/blocking')) {
+        const secs = req.url.substring(1 + req.url.indexOf('?'));
+        // blocking thread code
+        ((y = 10000, x = Date.now()) => { while (Date.now() - x < y) ;})();
+        return setTimeout(() => res.end(`<h1>Miliseconds: ${secs}ms</h1>\n`), secs);
     } else {
         res.write('OK\n');
     }
 
     res.end();
-    
+
 }).listen(8080);
